e634fb3a684eea361c2d12c06875d8ca5cf56e2c
yacodev <cyaco33@gmail.com>
2025-03-12 22:14:45 -0500

    add product-card component

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,9 @@
+import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {}
