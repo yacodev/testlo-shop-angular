711291bd94d80d8a0b049a73c6d8bf707cf21368
yacodev <cyaco33@gmail.com>
2025-03-22 21:10:08 -0500

    config to deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import {
@@ -13,7 +13,7 @@ import { authInterceptor } from './auth/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(
       withFetch(),
       withInterceptors([loggingInterceptor, authInterceptor])
